07872bd3c99dff8b51485f2e8b04f63b74210d3b
Victor Gonzalez <victorgonvilla@gmail.com>
2020-04-29 17:56:33 +0100

    adding server-side validation

--- GigHub/GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -1,24 +1,29 @@
 ﻿using GigHub.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
         public string Date { get; set; }
+
+        [Required]
         public string Time { get; set; }
+
+        [Required]
         //Int value of the current list selected option
         public byte Genre { get; set; }
 
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime
+        public DateTime GetDateTime()
         {
-            get
-            {
-                return DateTime.Parse(String.Format("{0} {1}", Date, Time));
-            }
+            return DateTime.Parse(String.Format("{0} {1}", Date, Time));
         }
     }
 }
