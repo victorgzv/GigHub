8a63362501e0f101e4c663d176c03eaa3063087d
Victor Gonzalez <victorgonvilla@gmail.com>
2020-04-29 17:17:00 +0100

    implement saving a gig

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -7,14 +9,13 @@ namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
-        // GET: Gigs
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
         public GigsController()
         {
             _context = new ApplicationDbContext();
         }
 
-
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -22,6 +23,27 @@ namespace GigHub.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(String.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
--- GigHub/GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -6,10 +6,11 @@ namespace GigHub.ViewModels
     public class GigFormViewModel
     {
         public string Venue { get; set; }
-        public int Genre { get; set; }
-
         public string Date { get; set; }
         public string Time { get; set; }
+        //Int value of the current list selected option
+        public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
 
     }
